79c4536e0d726465ba5b061a3221fd495ec76bbf
TobyH9 <toby.hallett9@gmail.com>
2025-08-16 16:00:14 +0100

    chore: delete unneccesary optimiser code

--- src/nanogpt/nanogpt.py
+++ src/nanogpt/nanogpt.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from transformer_layer import TransformerLayer
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 BASE_DIR = Path(__file__).parent.parent.parent
 
@@ -61,17 +61,23 @@ class NanoGPT(nn.Module):
         self.lm_head = nn.Linear(n_embed, vocab_size)
         self.optimiser = torch.optim.AdamW(self.parameters(), lr=1e-3)
 
-    def forward(self, batch= torch.Tensor, targets=None):
-        B, T = batch.shape # B = batch_size, T = block_size
+    def forward(self, batch=torch.Tensor, targets=None):
+        B, T = batch.shape  # B = batch_size, T = block_size
         C = self.n_embed
         # batch and targets are both (B,T) tensor of integers
-        tok_emb = self.token_embedding_table(batch) # replaces each token (int) in each sequence with an embedding vector such that tok_emb is (B,T,C)
-        pos_emb = self.positional_embedding_table(torch.arange(T, device=device)) # (T,C)
+        tok_emb = self.token_embedding_table(
+            batch
+        )  # replaces each token (int) in each sequence with an embedding vector such that tok_emb is (B,T,C)
+        pos_emb = self.positional_embedding_table(
+            torch.arange(T, device=device)
+        )  # (T,C)
         x = (
             tok_emb + pos_emb
         )  # Note torch uses broadcasting to convert pos_emb (T,C) to pos_emb (B,T,C) for this calculation (stacks B copies aling dimension 1)
         x = self.layers(x)  # attention, feedforward layers, (B,T,C)
-        logits = self.lm_head(x) # (B,T,vocab_size) if L = nn.Linear(input_dim, output_dim), W = (output_dim, input_dim) and b = (output_dim)
+        logits = self.lm_head(
+            x
+        )  # (B,T,vocab_size) if L = nn.Linear(input_dim, output_dim), W = (output_dim, input_dim) and b = (output_dim)
         if targets is None:
             loss = None
         else:
@@ -114,5 +120,3 @@ class NanoGPT(nn.Module):
             out[split] = losses.mean()
         model.train()
         return out
-
-        optimiser = torch.optim.AdamW(self.parameters(), lr=1e-3)
